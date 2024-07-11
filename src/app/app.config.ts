7da6e8535aef638d32010433627576ebd481f43c
Zishuo <fourcases1408@outlook.com>
2024-07-11 00:28:25 -0500

    initialized angularfire 18

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,33 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core'
+import { provideRouter } from '@angular/router'
 
-import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { routes } from './app.routes'
+import { provideClientHydration } from '@angular/platform-browser'
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app'
+import { getAuth, provideAuth } from '@angular/fire/auth'
+import { getFirestore, provideFirestore } from '@angular/fire/firestore'
+import { getFunctions, provideFunctions } from '@angular/fire/functions'
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideAnimationsAsync()]
-};
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(),
+    provideAnimationsAsync(),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'movie-server-e7170',
+        appId: '1:84149735552:web:627e818ab5a6a555359665',
+        storageBucket: 'movie-server-e7170.appspot.com',
+        apiKey: 'AIzaSyDYb2q6GjDXWnS1emZHCbDqV-S-wsqqoDU',
+        authDomain: 'movie-server-e7170.firebaseapp.com',
+        messagingSenderId: '84149735552',
+        measurementId: 'G-VGR2TE87MF',
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideFunctions(() => getFunctions()),
+  ],
+}
