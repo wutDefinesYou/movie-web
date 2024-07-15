132e6d41a768a29c8c7f8e3449a55202e2fd03a2
Zishuo <fourcases1408@outlook.com>
2024-07-15 02:26:16 -0500

    added home button in the header

--- src/app/features/header/header.component.ts
+++ src/app/features/header/header.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core'
 import { MatToolbarModule } from '@angular/material/toolbar'
 import { MatIconModule } from '@angular/material/icon'
 import { MatButtonModule } from '@angular/material/button'
+import { RouterModule } from '@angular/router'
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule, MatIconModule, MatButtonModule],
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule, RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
